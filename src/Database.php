c0868ffec66ff3079aeeef9f589b655991da5ab3
Orhan Tozan <nahro.tozan@gmail.com>
2018-06-18 23:05:39 +0200

    Initial commit

---
+++ src/Database.php
@@ -0,0 +1,38 @@
+<?php
+class Database {
+    
+    /** @var string */
+    private $host;
+
+    /** @var string */
+    private $username;
+
+    /** @var string */
+    private $password;
+
+    /** @var string */
+    private $name;
+
+    public function __construct(string $host, string $username, string $password, string $name) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->name = $name;
+    }
+
+    public function getHost(): string {
+        return $this->host;
+    }
+
+    public function getUsername(): string {
+        return $this->username;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+}
---
+++ src/DatabaseManager.php
@@ -0,0 +1,95 @@
+<?php
+class DatabaseManager {
+
+    /** @var Database The database this class uses. */
+    private $database;
+
+    /** @var PDO */
+    private $pdo;
+
+    /** @var array */
+    private $pdoAttributes = [];
+
+    public function __construct(Database $database) {
+        $this->database = $database;
+    }
+
+    /**
+     * Starts a new connection with the database.
+     * Must be called before {@see DatabaseManager::query()}
+     */
+    public function start(): void {
+        $database = $this->getDatabase();
+
+        $databaseHost = $database->getHost();
+        $databaseUsername = $database->getUsername();
+        $databasePassword = $database->getPassword();
+        $databaseName = $database->getName();
+
+        $pdo = new PDO(
+            "mysql:host=$databaseHost;dbname=$databaseName",
+            $databaseUsername,
+            $databasePassword
+        );
+
+        foreach ($this->pdoAttributes as $attribute => $value) {
+            $pdo->setAttribute($attribute, $value);
+        }
+
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * Queries the database.
+     * @param string $sqlQuery The query in SQL.
+     * @param array $bindedParameters (optional) Key-value pair(s) of binded parameters.
+     * @return array The query results.
+     */
+    public function query(string $sqlQuery, array $bindedParameters = null): array  {
+        if (is_null($this->pdo)) {
+            throw new Exception("query(..) called without calling start() before.");
+        }
+        $pdoStatement = $pdo->prepare($sqlQuery);
+        if (!is_null($bindedParameters)) {
+            foreach ($bindedParameters as $paramKey => $paramValue) {
+                $pdoStatement->bindParam($paramKey, $paramValue);
+            }
+        }
+        $pdoStatement->execute();
+        return $pdoStatement->fetchAll();
+    }
+
+    /**
+     * Stops the database connection.
+     * @see DatabaseManager::start()
+     */
+    public function stop(): void {
+        $this->pdo = null;
+    }
+
+    public function setDatabase(Database $database): void {
+        $this->database = $database;
+    }
+
+    /**
+     * @return Database The database.
+     */
+    public function getDatabase(): Database {
+        return $this->$database;
+    }
+
+    /**
+     * Sets the pdo attributes. {@see DatabaseManager::start()} needs to be called
+     * afterwards in order to apply the changes.
+     */
+    public function setPdoAttributes(array $pdoAttributes): void {
+        $this->pdoAttributes = $pdoAttributes;
+    }
+
+    /**
+     * @return array The configured PDO attributes.
+     */
+    public function getPdoAttributes(): array {
+        return $this->pdoAttributes;
+    }
+}
